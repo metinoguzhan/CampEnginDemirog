59775c7be39e31fc1a3fcee977160174ca4d811e
Metin Oguzhan <isslandbul@hotmail.com>
2021-08-16 00:44:57 +0300

    Engin Demirog Camp

---
+++ 02_ClassIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
