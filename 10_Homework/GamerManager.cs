2e0e0dec22dfe2136bed90a74b305632926a6b45
Metin Oguzhan <isslandbul@hotmail.com>
2021-08-30 13:33:02 +0300

    Engin Demirog

---
+++ 10_Homework/GamerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Homework
+{
+    class GamerManager : IGamerService
+    {
+        private IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer))
+            {
+                Console.WriteLine("Kayıt olundu");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama başarısız kayıt başarısız..");
+            }
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt güncellendi");
+        }
+
+        public void Delete(Gamer gmaer)
+        {
+            Console.WriteLine("Kayıt silindi");
+        }
+    }
+}
---
+++ 10_Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer()
+            {
+                Id = 1,
+                BirthYear = 1986,
+                IdentityNumber = 123456,
+                LastName = "Demiroğ",
+                FirstName = "Engin"
+            });
+
+            GamerManager gamerManager2 = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager2.Add(new Gamer()
+            {
+                Id = 1,
+                BirthYear = 1985,
+                IdentityNumber = 123456,
+                LastName = "Demiroğ",
+                FirstName = "Engin"
+            });
+        }
+    }
+}
